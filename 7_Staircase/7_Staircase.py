599487226cd0c2934c35c75f2cf9bc64616f1c98
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-01-17 18:27:40 +0900

    파이썬을 이용하니 엄청 간단하게 짜여졌다

--- 7_Staircase/7_Staircase.py
+++ 7_Staircase/7_Staircase.py
@@ -8,6 +8,9 @@ import sys
 
 # Complete the staircase function below.
 def staircase(n):
+    for i in range(1,n+1):
+        print(' '*(n-i),end='')
+        print('#'*(i))
 
 if __name__ == '__main__':
     n = int(input())

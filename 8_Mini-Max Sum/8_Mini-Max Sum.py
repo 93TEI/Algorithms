d001e5b2b665a28df5867099645db95dbabaabd3
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-01-22 18:01:26 +0900

    오류 아직 못잡음

--- 8_Mini-Max Sum/8_Mini-Max Sum.py
+++ 8_Mini-Max Sum/8_Mini-Max Sum.py
@@ -8,6 +8,9 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
+    max = sum(arr) - int((min(arr))
+    min = sum(arr) - int((max(arr))
+    return [max, min]
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))

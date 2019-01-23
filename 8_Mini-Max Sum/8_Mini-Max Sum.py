284ee028bd14d694b7747b1f1b92dbc83effa106
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-01-23 18:48:37 +0900

    input이 해커랭크에는 있는데 그 코드를 그대로 vs코드에서 쓰니까 오류가 났었던 것

--- 8_Mini-Max Sum/8_Mini-Max Sum.py
+++ 8_Mini-Max Sum/8_Mini-Max Sum.py
@@ -8,9 +8,9 @@ import sys
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    max = sum(arr) - int((min(arr))
-    min = sum(arr) - int((max(arr))
-    return [max, min]
+    a = sum(arr) - min(arr)
+    b = sum(arr) - max(arr)
+    print(b,a)
 
 if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))

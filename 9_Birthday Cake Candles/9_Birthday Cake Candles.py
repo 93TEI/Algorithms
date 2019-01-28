05532d6d710364c84f89e5c046297e400f864e84
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-01-28 11:20:51 +0900

    split함수 사용

--- 9_Birthday Cake Candles/9_Birthday Cake Candles.py
+++ 9_Birthday Cake Candles/9_Birthday Cake Candles.py
@@ -1,25 +1,26 @@
-#!/bin/python3
-
-import math
 import os
-import random
-import re
 import sys
 
-# Complete the birthdayCakeCandles function below.
-def birthdayCakeCandles(ar):
-    CanBlowsOut = max(ar)
-    return ar.count(CanBlowsOut)
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s):
+    split_of_s = s.split(':')
+    if split_of_s[2][2] =='A' and split_of_s[0]=="12" :
+        split_of_s[0]="00"
 
+    elif split_of_s[2][2] == 'P' and split_of_s[0]=="12":
+        pass
+    elif split_of_s[2][2] == 'P':
+        split_of_s[0]=str(int(split_of_s[0]) + 12)
+        
+    result = split_of_s[0]+':'+split_of_s[1]+':'+split_of_s[2][0]+split_of_s[2][1]
+    return result
+    
+    
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = birthdayCakeCandles(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+    s = input()
+    result = timeConversion(s)
+    f.write(result + '\n')
+    f.close()

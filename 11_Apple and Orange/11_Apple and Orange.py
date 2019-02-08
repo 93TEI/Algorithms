e99f3da05e6cd41942a65c826afc7edc8f40bafe
DESKTOP-UGITS1R\TEI <93louis@gmail.com>
2019-02-08 16:02:02 +0900

    집 바운더리 안으로 들어온 것만 체크하는 것이 아니라 밖으로 나갔는지도 체크해야하는 것이 중요했다.

--- 11_Apple and Orange/11_Apple and Orange.py
+++ 11_Apple and Orange/11_Apple and Orange.py
@@ -6,28 +6,21 @@ import sys
 
 # Complete the countApplesAndOranges function below.
 def countApplesAndOranges(s, t, a, b, apples, oranges):
+    new_a = [i for i in apples if i+a >= s and i+a<=t]
+    new_b = [i for i in oranges if i+b <= t and i+b>=s]
+    print(len(new_a))
+    print(len(new_b))
 
 if __name__ == '__main__':
-    st = input().split()
-
-    s = int(st[0])
-
+    st = input().split() # 집 [s,t]
+    s = int(st[0]) 
     t = int(st[1])
-
-    ab = input().split()
-
+    ab = input().split() # 사과나무, 오렌지나무 [a,b]
     a = int(ab[0])
-
     b = int(ab[1])
-
-    mn = input().split()
-
+    mn = input().split() # 개수
     m = int(mn[0])
-
     n = int(mn[1])
-
     apples = list(map(int, input().rstrip().split()))
-
     oranges = list(map(int, input().rstrip().split()))
-
     countApplesAndOranges(s, t, a, b, apples, oranges)

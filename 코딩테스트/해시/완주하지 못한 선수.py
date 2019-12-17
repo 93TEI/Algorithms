d39ea16d6f3a6cb41d1f65a747f80d29a5167a25
93TEI <93louis@gmail.com>
2019-12-17 18:32:47 +0900

    진짜 효율적으로 잘짰다고 생각했는데 안돌아감

--- 코딩테스트/해시/완주하지 못한 선수.py
+++ 코딩테스트/해시/완주하지 못한 선수.py
@@ -1,6 +1,5 @@
 # 완주하지 못한 선수
 def solution(participant, completion):
     for i in range(0,len(participant)):
-        completion.pop(completion.index(participant[i]))
-    answer = completion
-    return answer
+        if completion.pop(completion.index(participant[i])) == FALSE:
+            return participant[i]

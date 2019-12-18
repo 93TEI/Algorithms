f3e05a94b96795c6811069b91f5f441da0f651bb
93TEI <93louis@gmail.com>
2019-12-18 23:18:42 +0900

    효율성 테스트도 통과하도록 바꿈. 시간복잡도를 생각하게 한 좋은 문제였다. resolved #6

--- 코딩테스트/해시/완주하지 못한 선수.py
+++ 코딩테스트/해시/완주하지 못한 선수.py
@@ -1,9 +1,13 @@
 # 완주하지 못한 선수
 def solution(participant, completion):
-    for i in range(0,len(participant)):
-        if not participant[i] in completion:
-            answer = participant[i]
-            break
-        else :
-            completion.pop(completion.index(participant[i]))
-    return answer
+    participant.sort()
+    completion.sort()
+    
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+
+# 풀기 위해 많은 것을 배웠다.
+# 튜플, 딕셔너리, 리스트를 다시 봄은 물론, 해시에 대해서도 알아보는 시간이 되었다.
+# 그중 시간복잡도에 대한 생각을 가지게 된 것이 가장 큰 수확인 것 같다.

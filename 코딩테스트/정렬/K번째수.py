e87da6997db8861d870484af66e4faa164c91bff
93TEI <93louis@gmail.com>
2019-12-20 15:14:53 +0900

    list index out of range 고칠 것

--- 코딩테스트/정렬/K번째수.py
+++ 코딩테스트/정렬/K번째수.py
@@ -1,4 +1,8 @@
 #K번째 수
 def solution(array, commands):
-    answer = []
+    answer=[]
+    for n in range(0,3):
+        temp=array[commands[n][0]:commands[n][1]+1]
+        temp.sort()
+        answer.append(temp[commands[n][2]])
     return answer

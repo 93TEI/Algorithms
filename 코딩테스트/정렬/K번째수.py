1ec6b344ed3ec4a42e0f839d5f02a67b21c9d5fb
93TEI <93louis@gmail.com>
2019-12-20 15:26:42 +0900

    solved

--- 코딩테스트/정렬/K번째수.py
+++ 코딩테스트/정렬/K번째수.py
@@ -1,8 +1,11 @@
 #K번째 수
 def solution(array, commands):
     answer=[]
-    for n in range(0,3):
-        temp=array[commands[n][0]:commands[n][1]+1]
+    for n in range(0,len(commands)):
+        temp=array[commands[n][0]-1:commands[n][1]] # 범위 실수
         temp.sort()
-        answer.append(temp[commands[n][2]])
+        answer.append(temp[commands[n][2]-1]) # 범위 2번째 실수
     return answer
+
+# 쉽다고 생각했는데 틀렸다.
+# 항상 자만하지 말고 정확히 푸는 습관을 들여야할 것

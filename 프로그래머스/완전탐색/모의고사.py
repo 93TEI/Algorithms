e947a986ccb066a9b459d656e98ac1286426da9a
93TEI <93louis@gmail.com>
2019-12-23 01:07:06 +0900

    1,2번 수포자 리스트까지 구현

--- 프로그래머스/완전탐색/모의고사.py
+++ 프로그래머스/완전탐색/모의고사.py
@@ -6,11 +6,27 @@ def solution(answers):
     n1 = []
     n2 = []
     n3 = []
-    temp=0
+    temp = 0
+    # n1 리스트 만들기
     for i in range(len(answers)) :
-        if temp == 6 :
-            temp = 0
-        n1.append(i+1+temp)
+        if i%5 == 0 :
+            temp += 1
+        n1.append(i+1-(temp*5))
+    
+    temp = 1
+    # n2 리스트 만들기
+    for i in range(len(answers)):
+        if (i+1)%2 == 1 :
+            n2.append(2)
+        else :
+            if temp == 6 :
+                temp = 1
+            n2.append(temp)
+            temp += 1
+    
+    # n3 리스트 만들기
+    for i in range(len(answers)) :
+        
         
     for i in range(len(answers)) :
         answers[i] 

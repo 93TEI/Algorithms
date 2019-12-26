b4ee4a665f35a793dfd0dd48a83ea3039e3658a6
93TEI <93louis@gmail.com>
2019-12-26 16:08:49 +0900

    코드 간략화

--- 프로그래머스/완전탐색/모의고사.py
+++ 프로그래머스/완전탐색/모의고사.py
@@ -1,63 +1,43 @@
 # 모의고사
 
 def solution(answers):
-    answer = []
     # 1,2,3번 수포자를 for문으로 미리 만들어서 비교할 생각
-    n1 = []
-    n2 = []
-    n3 = []
-    temp = 0
-    n1_cnt = 0
-    n2_cnt = 0
-    n3_cnt = 0
+    n1,n2,n3,answer= [],[],[],[]
+    n1_cnt, n2_cnt, n3_cnt,temp = 0,0,0,0
     
     # n1 리스트 만들기
-    n1_list = [1,2,3,4,5]
+    n_list = [1,2,3,4,5]
     for i in range(0,len(answers)+1) :
         if i%5 == 0 :
             temp+=1
-        n1.append(n1_list[i-(temp*5)])
+        n1.append(n_list[i-(temp*5)])
 
-    temp = 1
     # n2 리스트 만들기
+    n_list, temp = [2,1,2,3,2,4,2,5], 0
     for i in range(len(answers)):
-        if (i+1)%2 == 1 :
-            n2.append(2)
-        else :
-            if temp == 6 :
-                temp = 1
-            elif temp == 2 :
-                temp += 1
-            n2.append(temp)
+        if i%8 == 0 :
             temp += 1
+        n2.append(n_list[i-(temp*8)])
     
     # n3 리스트 만들기
+    n_list, temp = [3,3,1,1,2,2,4,4,5,5], 0
     for i in range(len(answers)) :
-        if (i+1)%10 == 1 or (i+1)%10 == 2 :
-            n3.append(3)
-        elif (i+1)%10 == 3 or (i+1)%10 == 4 :
-            n3.append(1)
-        elif (i+1)%10 == 5 or (i+1)%10 == 6 :
-            n3.append(2)
-        elif (i+1)%10 == 7 or (i+1)%10 == 8 :
-            n3.append(4)
-        else :
-            n3.append(5)
+        if i%10 == 0 :
+            temp += 1
+        n3.append(n_list[i-(temp*10)])
     
     # 각각 수포자들이 몇개 맞았는지 확인
     for i in range(len(answers)) :
         if answers[i] == n1[i] :
-            n1_cnt += n1_cnt + 1
+            n1_cnt += 1
         if answers[i] == n2[i] :
-            n2_cnt += n2_cnt + 1
+            n2_cnt += 1
         if answers[i] == n3[i] :
-            n3_cnt += n3_cnt + 1
-    
-    # 리스트화 시키고 max를 뽑아냄    
-    n_cnt = [n1_cnt, n2_cnt, n3_cnt]
-    max_cnt = max(n_cnt)
+            n3_cnt += 1
     
     # max와 같은 값을 가진 수포자를 answer 리스트에 append
+    n_cnt = [n1_cnt, n2_cnt, n3_cnt]
+    max_cnt = max(n_cnt)
     for i in range(3) :
         if max_cnt == n_cnt[i] :
             answer.append(i+1)

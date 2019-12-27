de810aef6fc5b531661eb738d7915ce97a9750cd
93TEI <93louis@gmail.com>
2019-12-27 21:07:33 +0900

    solved

--- 프로그래머스/연습문제/문자열 내림차순으로 배치하기.py
+++ 프로그래머스/연습문제/문자열 내림차순으로 배치하기.py
@@ -1,15 +1,13 @@
 # 문자열 내림차순으로 배치하기
 def solution(s):
-    answer = s
-    temp_answer, cnt = '', 0 # 임시변수
+    answer = list(s)
+    result = ''
     for i in range(len(s)-1) : # 정렬
-        if answer[i] > answer[i+1] :
-            temp = answer[i]
-            answer[i] = answer[i+1]
-            answer[i+1] = i
-    for i in range(len(s)) : # 대문자는 뒤로 빼서 다시 합치기
-        if int(answer[i]) >= 65 and int(answer[i]) <= 90 :
-            temp_answer += answer[i]
-            cnt += 1
-    result = answer[cnt:]+temp_answer
+        for j in range(len(s)-1) :
+            if ord(answer[j]) < ord(answer[j+1]) :
+                temp = answer[j]
+                answer[j] = answer[j+1]
+                answer[j+1] = temp
+    for i in range(len(answer)) : # 리스트였기 때문에 다시 문자열로 만들어서 return
+        result += answer[i]
     return result

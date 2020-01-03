116b256a5351e8aba8824a1c2811333cbc0403de
93TEI <93louis@gmail.com>
2020-01-03 17:54:04 +0900

    프로그래머스 코드 채점 결과 90.0%, 1개 케이스 실패 고칠 것

--- 프로그래머스/탐욕법(Greedy)/조이스틱.py
+++ 프로그래머스/탐욕법(Greedy)/조이스틱.py
@@ -1,7 +1,7 @@
 # 조이스틱 // 조작 횟수의 최소값을 return
 def solution(name):
     n = list(name)
-    j, cnt_A = 0, 0
+    j_temp = 0
     n1 = min(abs(ord(n[0]) - 65), abs(91 - ord(n[0]))) # 첫 글자
     answer1, answer2 = n1, n1 # 오름차순, 내림차순 판단하기 위해 두 가지 케이스로 나눔
     # 오른차순
@@ -10,9 +10,8 @@ def solution(name):
         answer1 += temp + 1
     for j in range(len(n)-1,0,-1) :
         if n[j] != 'A' : break
-    for k in range(1,j) :
-        if n[k] == 'A' : cnt_A += 1
-    answer1 -= (len(n)-1-j) + cnt_A
+        j_temp += 1
+    answer1 -= (len(n)-1-j)
     cnt_A = 0
     # 내림차순
     for i in range(len(n)-1,0,-1):
@@ -20,13 +19,12 @@ def solution(name):
         answer2 += temp + 1
     for j in range(1,len(n)) :
         if n[j] != 'A' : break
-    for k in range(len(n)-1,j,-1) :
-        if n[k] == 'A' : cnt_A += 1
-    answer2 -= j-1 + cnt_A
+        j_temp += 1
+    answer2 -= j-1
     answer = min(answer1,answer2)
     return answer
 
-
+# 4
 # len(name)만큼 for문 돌리고
 # 그 안에서 각 글자마다 최소값을 판단 (가중치)
 # 첫번째, 첫글자 최소값
